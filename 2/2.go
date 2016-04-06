4622f48e5e77a731bdb77d26f7a429d807d56888
童中波 <tongzhongbo@Tong-Zhongbo.local>
2016-04-06 11:12:43 +0800

    compare sort Algorithms

--- 2/2.go
+++ 2/2.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"reflect"
+	"runtime"
 	"time"
 )
 
@@ -13,12 +16,21 @@ func sort(f func([]int), times int, length int) {
 		f(arr)
 	}
 	t1 := time.Now()
-	fmt.Printf("The %v took %v to run.\n", f, t1.Sub(t0))
+	fmt.Printf("The %v took %v to run.\n", runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name(), t1.Sub(t0))
 }
 
 func main() {
+	funcs := map[string]func([]int){"improv_insertion": improv_insertion, "insertion": insertion, "selection": selection}
 
-	sort(improv_insertion, 1000, 1000)
-	sort(insertion, 1000, 1000)
-	sort(selection, 1000, 1000)
+	len := len(os.Args)
+	if len < 2 {
+		fmt.Println("Please special sort algorthms")
+		os.Exit(2)
+	}
+
+	for i := 1; i < len; i++ {
+		if fun, ok := funcs[os.Args[i]]; ok {
+			sort(fun, 1, 100)
+		}
+	}
 }
