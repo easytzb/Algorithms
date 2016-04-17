a49d53cbf140d14ea448bf80f5a7ece7b3e9fcf9
童中波 <tongzhongbo@Tong-Zhongbo.local>
2016-04-18 06:48:27 +0800

    代码验证希尔排序比较次数与数组长度比例是个小常数

--- 2/shellbars.go
+++ 2/shellbars.go
@@ -1,13 +1,16 @@
 //shellBars sort
 package main
 
+//import "fmt"
 func shellBars(arr []int) {
 	len := len(arr)
+	//compareNums := 0
 	for step := len >> 1; step > 0; step = step >> 1 {
 		for i := step; i < len; i++ {
 			tmp := arr[i]
 			tmp_index := i
 			for j := i - step; j >= 0 && arr[j] > tmp; j = j - step {
+				//compareNums++
 				arr[j+step] = arr[j]
 				tmp_index = j
 			}
@@ -16,4 +19,6 @@ func shellBars(arr []int) {
 			}
 		}
 	}
+
+	//fmt.Println(compareNums / len)
 }
